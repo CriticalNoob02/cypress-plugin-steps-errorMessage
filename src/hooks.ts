972b4e69891ba9beae4c1a9e8a25fe9ef4de85bd
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-03-06 09:05:57 -0300

    feat: Add logic for error logging when not using the plugin

--- src/hooks.ts
+++ src/hooks.ts
@@ -7,13 +7,17 @@ beforeEach(function () {
   window.errorText = [];
 });
 
+
 Cypress.on('fail', (err) => {
   console.log(err)
-  if(!window.errorText.length){
-    err.message += `${'\n\n' + 'Test steps were:\n\n'}${window.testFlow.join('\n')}`;
+  if(!window.errorText.length && !window.testFlow.length){
+    err.message += `${'\n\n' + 'Arquivo de teste sem cobertura de erro!\n\n'}`
+  }
+  else if(!window.testFlow.length){
+    err.message += `${'\n\n' + 'Possivel erro:\n\n'}${window.errorText.pop()}\n\n`
   }
   else{
-    err.message += `${'\n\n' + 'Error:\n\n'}${window.errorText.pop()}\n\n`;
+    err.message += `${'\n\n' + 'Steps de teste:\n\n'}${window.testFlow.join('\n')}`
   }
   throw err;
 });
